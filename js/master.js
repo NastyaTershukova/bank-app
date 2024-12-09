06db7e412a98202d0588620b6bf311814cdf2da9
NastyaTershukova <137862452+NastyaTershukova@users.noreply.github.com>
2024-12-09 15:02:15 +0400

    glinda elphaba

---
+++ js/master.js
@@ -0,0 +1,41 @@
+document.querySelector('.section-button.white.sign-up').addEventListener('click', function(){
+    document.querySelector('.start-section').style.display = "none";
+    document.querySelector('.section.sign-up').style.display = "flex";
+});
+
+document.querySelector('.section-button.blue.continue').addEventListener('click', function(){
+    document.querySelector('.section.sign-up').style.display = "none";
+    document.querySelector('.section.enter-code').style.display = "flex";
+});
+
+document.querySelector('.section-button.blue.next').addEventListener('click', function(){
+    document.querySelector('.section.enter-code').style.display = "none";
+    document.querySelector('.section.create-passcode').style.display = "flex";
+});
+
+let slides = document.querySelectorAll('.start-section_content');
+let currentSlide = 0;
+let interval;
+
+function showSlide(number) {
+    if (document.querySelector('.start-section_content.active')) {
+        document.querySelector('.start-section_content.active').classList.remove('active');
+    }
+    slides[number].classList.add('active');
+    restartProgressBar(number);
+}
+showSlide(0);
+
+function startSpinningSlides() {
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => {
+        currentSlide = currentSlide + 1;
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+        showSlide(currentSlide);
+    }, 5000);
+}
+startSpinningSlides();
